46f5f73ccb6e972b9a2e13cf3b9ed51fab6bff2a
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2025-02-05 01:21:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def plot_and_export_chart(df, x_col, y_col, color_col, size_col, opacity_col, ra
             opacities = np.full(len(selected_data), (min_opacity + max_opacity) / 2)
 
         # Create the figure and axes
-        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(fig_width, fig_height), gridspec_kw={'width_ratios': [0.6, 2.8]})
+        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(fig_width, fig_height), gridspec_kw={'width_ratios': [0.6, 4.8]})
 
 
         # Plot the scatter points
